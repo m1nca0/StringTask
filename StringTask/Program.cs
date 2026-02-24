94e43448ea1f276cc9173cc23b09871170a24194
m1nca0 <noname890256@gmail.com>
2026-02-24 11:19:05 +0800

    Добавьте файлы проекта.

---
+++ StringTask/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace StringTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string sentence = Console.ReadLine();
+            char[] alphabetSentence = new char[15];
+            int[] alphabetcounter = new int[15];
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                alphabetSentence[i] = sentence[i];
+            }
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                alphabetcounter[i] = sentence.Count(alphabetSentence[i]);
+            }
+
+        }
+    }
+}
