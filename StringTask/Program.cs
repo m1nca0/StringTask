5cd1eb069dfee8c63c1c57e070e5397678b4fb71
m1nca0 <noname890256@gmail.com>
2026-02-24 14:30:20 +0800

    add Logic class

--- StringTask/Program.cs
+++ StringTask/Program.cs
@@ -1,6 +1,28 @@
 ﻿using System.Linq;
 namespace StringTask
 {
+    public class Logic
+    {
+        public static char[] MakeAlphabetSentence(string sentence)
+        {
+            char[] alphabetSentence = new char[100];
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                alphabetSentence[i] = sentence[i];
+            }
+            alphabetSentence = alphabetSentence.Distinct().ToArray();
+            return alphabetSentence;
+        }
+        public static int[] MakeAlphabetCounter(string sentence, char[] alphabetSentence)
+        {
+            int[] alphabetcounter = new int[100];
+            for (int i = 0; i < alphabetSentence.Length; i++)
+            {
+                alphabetcounter[i] = sentence.Count(alphabetSentence[i]);
+            }
+            return alphabetcounter;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -8,17 +30,9 @@ namespace StringTask
             Console.WriteLine("Введите строку:");
             string sentence = Console.ReadLine();
             sentence = sentence.Replace(" ", "");
-            char[] alphabetSentence = new char[15];
-            int[] alphabetcounter = new int[15];
-            for (int i = 0; i < sentence.Length; i++)
-            {
-                alphabetSentence[i] = sentence[i];
-            }
-            alphabetSentence = alphabetSentence.Distinct().ToArray();
-            for (int i = 0; i < alphabetSentence.Length; i++)
-            {
-                alphabetcounter[i] = sentence.Count(alphabetSentence[i]);
-            }
+            char[] alphabetSentence = Logic.MakeAlphabetSentence(sentence);
+            int[] alphabetcounter = Logic.MakeAlphabetCounter(sentence, alphabetSentence);
+
             for (int i = 0; i < alphabetSentence.Length; i++)
             {
                 Console.WriteLine(alphabetSentence[i]  + " " + alphabetcounter[i] * 100 / sentence.Length + "%");
